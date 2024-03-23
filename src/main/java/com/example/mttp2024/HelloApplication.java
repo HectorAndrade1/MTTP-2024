124587272e89e596b2ea539bf32e62e9969f3851
K-N-M-H <162653128+K-N-M-H@users.noreply.github.com>
2024-03-22 22:36:29 -0400

    TareaController

--- src/main/java/com/example/mttp2024/HelloApplication.java
+++ src/main/java/com/example/mttp2024/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.mttp2024;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(),854,480);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
