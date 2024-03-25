eb648aad3cb821e45abf718f2c141e24f9cede8f
K-N-M-H <162653128+K-N-M-H@users.noreply.github.com>
2024-03-25 09:19:12 -0400

    EventoController

--- src/main/java/com/example/mttp2024/vistacontroller/MenuController.java
+++ src/main/java/com/example/mttp2024/vistacontroller/MenuController.java
@@ -3,6 +3,7 @@ package com.example.mttp2024.vistacontroller;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.*;
 
 public class MenuController {
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.mttp2024 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.graphics;
 
 
     opens com.example.mttp2024.vistacontroller to javafx.fxml;
