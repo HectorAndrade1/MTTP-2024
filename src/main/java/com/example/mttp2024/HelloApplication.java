d96a5d8224b37cb593c6f95e24e1884932267dba
K-N-M-H <162653128+K-N-M-H@users.noreply.github.com>
2024-03-24 23:09:43 -0400

    EventoController

--- src/main/java/com/example/mttp2024/HelloApplication.java
+++ src/main/java/com/example/mttp2024/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),854,480);
-        stage.setTitle("Hello!");
+        stage.setTitle("Organizador de Tareas");
         stage.setScene(scene);
         stage.show();
     }
