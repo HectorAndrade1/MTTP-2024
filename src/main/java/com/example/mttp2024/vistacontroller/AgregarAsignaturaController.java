adc85068666c2d45625e4aa4b52dfb29bcd3a145
K-N-M-H <162653128+K-N-M-H@users.noreply.github.com>
2024-03-25 10:21:24 -0400

    EventoController

--- src/main/java/com/example/mttp2024/vistacontroller/AgregarAsignaturaController.java
+++ src/main/java/com/example/mttp2024/vistacontroller/AgregarAsignaturaController.java
@@ -2,8 +2,16 @@ package com.example.mttp2024.vistacontroller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class AgregarAsignaturaController {
+
+    @FXML
+    private Button botonguardarasignatura;
+
+    @FXML
+    private TextField nombreasignatura;
 
     @FXML
     void btnGuardarAsignaturaClick(ActionEvent event) {
@@ -12,7 +20,7 @@ public class AgregarAsignaturaController {
 
     @FXML
     void textoAgregarAsignaturaNombre(ActionEvent event) {
-            String texto=event.toString();
+
     }
 
 }
---
+++ src/main/java/com/example/mttp2024/vistacontroller/AgregarEventoController.java
@@ -0,0 +1,32 @@
+package com.example.mttp2024.vistacontroller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AgregarEventoController {
+
+    @FXML
+    private TextField descripcionEvento;
+
+    @FXML
+    private TextField fechaEvento;
+
+    @FXML
+    private TextField horaFin;
+
+    @FXML
+    private TextField horaInicio;
+
+    @FXML
+    private TextField nombreEvento;
+
+    @FXML
+    private TextField ubicacionEvento;
+
+    @FXML
+    void btnGuardarEventoButtonClick(ActionEvent event) {
+
+    }
+
+}
